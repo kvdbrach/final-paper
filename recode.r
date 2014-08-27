f3b34f76b18a49c33029f373fd341cd783e3a566
kvdbrach <koen.vanderbracht@ugent.be>
2014-08-27 15:37:58 +0200

    new file:   .RData 	new file:   recode.r

---
+++ recode.r
@@ -0,0 +1,20 @@
+###Change DataFilenames if needed
+ess<-ess
+gss<-gss
+eds<-eds
+
+
+###Praying 
+###Omzetten naar 5-puntenschaal van EDS
+###Categorieën:0: Never 1: Less than once a week 2: At least once a week
+
+###pray-verdeling checken bij GSS
+
+eds$praying<-ifelse(as.numeric(eds$BK_Q135)>5,NA,as.numeric(eds$BK_Q135))
+eds$praying<-ifelse(eds$praying>1,0,1)
+
+ess$praying<-ifelse(as.numeric(ess$pray)>4,0,1)
+
+gss$praying<-ifelse(as.numeric(gss$pray)>4,0,1)
+
+
