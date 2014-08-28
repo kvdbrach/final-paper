88de9a9fe2c7eb6a6b3a051d63daeaa67dda7af5
kvdbrach <koen.vanderbracht@ugent.be>
2014-08-28 12:01:37 +0200

    modified:   recode.r

--- recode.r
+++ recode.r
@@ -3,12 +3,14 @@ ess<-ess
 gss<-gss
 eds<-eds
 
+###Add constants to each dataset
+ess$cons<-1
+gss$cons<-1
+eds$cons<-1
 
 ###Praying 
-###Omzetten naar 5-puntenschaal van EDS
-###Categorieën:0: Never 1: Less than once a week 2: At least once a week
-
-###pray-verdeling checken bij GSS
+###Convert to dichotomous variables
+###Categories:0: Less than once a week 2: At least once a week
 
 eds$praying<-ifelse(as.numeric(eds$BK_Q135)>5,NA,as.numeric(eds$BK_Q135))
 eds$praying<-ifelse(eds$praying>1,0,1)
@@ -17,5 +19,21 @@ ess$praying<-ifelse(as.numeric(ess$pray)>3,0,1)
 
 gss$praying<-ifelse(as.numeric(gss$pray)>4,0,1)
 
-###Subjective-religiosity
+###Affiliation
+eds$RELSUM<-ifelse(as.numeric(eds$RELSUM)>12,NA,eds$RELSUM)
+eds$affiliated<-ifelse(eds$RELSUM==1,0,1)
+
+gss$affiliated<-ifelse(as.numeric(gss$relig)==4,0,1)
+
+ess$affiliated<-ifelse(as.numeric(ess$rlgblg)==2,0,1)
+
+
+###Migrants
+ess$migr<-ifelse(ess$brncntr=='Yes',ifelse((ess$facntr=='Yes' & ess$mocntr=='Yes'),0,2),ifelse((ess$facntr=='Yes' | ess$mocntr=='Yes'),0,1))
+
+###Destination
+levels(ess$cntry)<-c(levels(ess$cntry),'United States','Canada')
+eds$countries<-factor('Canada',levels=levels(ess$cntry))
+gss$countries<-factor('United States',levels=levels(ess$cntry))
+ess$countries<-ess$cntry
 
